eba68a39bf3673e2d5e1cea259c4aabb0da4b30a
ivan-chekunkov <chekunkov.ivan@yandex.ru>
2023-12-08 16:06:46 +0300

    models

--- bot/wiki/admin.py
+++ bot/wiki/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Location
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        "date_created",
+        "name",
+        "description",
+    )
+    search_fields = ("name",)
+    list_filter = ("date_created",)
+    empty_value_display = "-пусто-"
+
+
+admin.site.register(Location, LocationAdmin)
--- bot/wiki/models.py
+++ bot/wiki/models.py
@@ -1,6 +1,67 @@
 from django.db import models
 
 
+class District(models.Model):
+    date_created = models.DateTimeField(
+        verbose_name="Дата создания записи",
+        auto_now_add=True,
+    )
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Наименование района",
+        help_text="Введите название района",
+    )
+    adjacent_district = models.ManyToManyField(
+        to="self",
+        through="Adjacent_district",
+        related_name="district",
+        blank=True,
+    )
+
+    class Meta:
+        db_table = "districts"
+        db_table_comment = "Table of city districts"
+        verbose_name = "Район"
+        verbose_name_plural = "Районы"
+        ordering = ["-date_created"]
+
+    def __str__(self) -> str:
+        return self.name[:10]
+
+
+class Adjacent_district(models.Model):
+    main_district = models.ForeignKey(
+        to=District,
+        on_delete=models.CASCADE,
+        related_name="main_district",
+        blank=True,
+    )
+    neighboring_district = models.ForeignKey(
+        to=District,
+        on_delete=models.CASCADE,
+        related_name="neighboring_district",
+        blank=True,
+    )
+
+    class Meta:
+        db_table = "adjacent_districts"
+        db_table_comment = "Table of adjacent areas"
+        verbose_name = "Соседний район"
+        verbose_name_plural = "Соседние районы"
+        ordering = ["main_district", "neighboring_district"]
+
+    def __str__(self) -> str:
+        return self.main_district[:10] + " " + self.neighboring_district[:10]
+
+
+class Picture(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Наименование изображения",
+        help_text="Введите название изображения",
+    )
+
+
 class Location(models.Model):
     date_created = models.DateTimeField(
         verbose_name="Дата создания записи",
